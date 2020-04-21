2ee579403e4ce51a2b47bd0af7d6c178243fa0eb
Dmitriy Medvedkin <dmitriyphc@gmail.com>
2020-04-22 01:36:19 +0300

    lesson 1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let money = prompt("Ваш бюджет на месяц", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
+    obligatory = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    howMany = prompt("Во сколько обойдется?", ""),
+    obligatory2 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    howMany2 = prompt("Во сколько обойдется?", ""),
+
+    appData = {
+        budget: money,
+        timeData: time,
+        expenses:{},
+        optionalExpenses:{},
+        income: [],
+        saving: false
+    };
+
+appData.expenses[obligatory] = howMany;
+appData.expenses[obligatory2] = howMany2;
+
+
+alert("Бюджет на день " + appData.budget/ 30 + " руб.");
