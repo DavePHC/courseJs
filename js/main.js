e930a6e49a4fab792644828d20faa7be54012e19
Dmitriy Medvedkin <dmitriyphc@gmail.com>
2020-05-11 17:37:15 +0300

    add new image

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let money = prompt("Ваш бюджет на месяц", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD", ""),
     obligatory = prompt("Введите обязательную статью расходов в этом месяце", ""),
